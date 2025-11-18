de60a764069d72c1b1fa3097238f28c20a7f10df
Arson <youaregifted@myyahoo.com>
2025-11-18 15:38:14 +0530

    fixed a typo

--- config.py
+++ config.py
@@ -1,20 +1,20 @@
 import os
 
-basedir = os.path.abspath(__file__)
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
 
         SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-secret-key-in-production'
 
-        SQLACHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'instance' , 'arson.db')
+        SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'instance' , 'arson.db')
 
-        SQLACHEMY_TRACK_MODIFICATION = False
+        SQLALCHEMY_TRACK_MODIFICATION = False
 
         UPLOAD_FOLDER = os.path.join(basedir,'app','static','uploads')
 
         MAX_CONTENT_LENGTH = 5 * 1024 * 1024  # 5mb
 
-        ALLOWED_EXTENSION = {
+        ALLOWED_EXTENSIONS = {
         'png','jpg','jpeg','gif', 'webp'
         }
 
