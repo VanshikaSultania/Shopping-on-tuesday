ab7f02a8747168d659a525a6a34cb6fe24aedb93
Arson <youaregifted@myyahoo.com>
2025-11-18 15:39:23 +0530

    Fixed a bug

--- app/__init__.py
+++ app/__init__.py
@@ -3,15 +3,21 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from config import config
+import os
 
-db = SQLAlchemy
-login_manager = LoginManager
+db = SQLAlchemy()
+login_manager = LoginManager()
 
 def create_app(config_name = 'default'):
 
     app = Flask(__name__)
 
     app.config.from_object(config[config_name])
+
+    instance_path = os.path.join(app.root_path,'..','instance')
+
+    if not os.path.exists(instance_path):
+        os.makedirs(instance_path)
 
     db.init_app(app)
     login_manager.init_app(app)
