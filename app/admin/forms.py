1decba9048451789c8ece54f83da7ac954c5f041
Arson <youaregifted@myyahoo.com>
2025-11-27 07:30:21 +0530

    Added 'add product form'

---
+++ app/admin/forms.py
@@ -0,0 +1,19 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, SelectField, IntegerField, FloatField , TextAreaField
+from wtforms.validators import DataRequired, NumberRange, Length, Optional
+
+class ProductForm(FlaskForm):
+
+    name = StringField('Product Name',validators=[DataRequired(message='Name of the product is required'),Length(min=3,max=50, message='Name must be between 3 to 50 characters')])
+
+    price = FloatField('Price of Product',validators=[DataRequired(message='Price is required'),NumberRange(min=1, message='Price of Product cant be negative')])
+
+    category = SelectField('Product Category',choices=[('anime','Anime'),('stationary','Stationary')],validators=[DataRequired(message='Category of product is required')])
+
+    stock = IntegerField('Stock',validators=[DataRequired(message='Stock is empty'),NumberRange(min=0, message='Stock cannot be negative')])
+
+    description = TextAreaField('Product Description',validators=[Length(max=1000, message='Word limit exceeds'),Optional])
+
+    image_url = StringField('Image URL',Optional)
+
+    submit = SubmitField('Add Product')
